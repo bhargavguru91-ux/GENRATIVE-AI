73af7d4793195ed56077ac981d94b1032fc94413
bhargavguru91-ux <bhargavguru91@gmail.com>
2026-08-21 14:43:33 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,30 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS 
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.documents import Document
+from pypdf import PdfReader
+# Resume Analyzer using RAG and LLM 
+
+# extracting pdf info --> RAG Document Lading extract
+def extract_pdf(file):
+    reader = PdfReader(file)
+    text = "" # str type
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+#spliiting 
+def split_text(text):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=50
+    )
+    return splitter.split_text(text)
+
+# embedding and vector storage
+def create_vector_text(text):
+    chunks = split_text(text)
+    docs = [Document(page_content=c) for c in chunks]
+    embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vectorstore = FAISS.from_documents(docs,embedding)
+    return vectorstore
